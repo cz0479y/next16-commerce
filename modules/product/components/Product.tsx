1675675db74b734bbd6f93e7a0f95b05fab6a10d
Aurora Scharff <aurora.sofie@gmail.com>
2025-08-01 17:21:45 +0200

    fix: fallback UI height

--- modules/product/components/Product.tsx
+++ modules/product/components/Product.tsx
@@ -32,7 +32,7 @@ export default async function Product({ productId, details, imageClassName }: Pr
 export function ProductSkeleton({ className, isDetails = false }: { className?: string; isDetails?: boolean }) {
   return (
     <div className={cn('dark:bg-card-dark flex flex-col bg-white', className)}>
-      <div className={cn('bg-card dark:bg-section w-full', isDetails ? 'h-96' : 'h-62')} />
+      <div className={cn('bg-card dark:bg-section w-full', isDetails ? 'h-96' : 'h-60')} />
       <Skeleton className="p-4" />
     </div>
   );
