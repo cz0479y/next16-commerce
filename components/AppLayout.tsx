e82ef23a9040391bb7bf647d2b553b7b98d46866
Aurora Scharff <aurora.sofie@gmail.com>
2025-08-04 15:55:53 +0200

    Prefetch home

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -11,7 +11,7 @@ export default function AppLayout({ headerContent, children }: Props) {
     <>
       <header className="border-divider dark:border-divider-dark flex min-h-20 items-center justify-between border-b bg-white px-4 py-4 sm:px-10 2xl:px-60 dark:bg-black">
         <h1 className="text-3xl font-bold">
-          <Link href="/" className="text-primary hover:text-primary-dark font-bold">
+          <Link prefetch href="/" className="text-primary hover:text-primary-dark font-bold">
             Commerce
           </Link>
         </h1>
