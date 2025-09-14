b6fff46021c52319ce7eed70d1c62a252558ad0c
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-14 20:57:51 +0200

    Try fix static

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,12 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import AppLayout from '@/components/layout/AppLayout';
 
 import { AuthProvider } from '@/features/auth/components/AuthProvider';
-import UserProfile, { UserProfileSkeleton } from '@/features/user/components/UserProfile';
 
 export default async function AuthLayout({ children, modal }: LayoutProps<'/'>) {
-  // const loggedIn = getIsAuthenticated();
-
   return (
     <AuthProvider loggedIn={Promise.resolve(false)}>
-      <AppLayout headerContent={<Suspense fallback={<UserProfileSkeleton />}>{<UserProfile />}</Suspense>}>
+      <AppLayout>
         {children}
         {modal}
       </AppLayout>
