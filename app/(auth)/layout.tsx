53f6364486b2deeb1042f2ff6a2fe8a1949cb11b
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-15 14:19:21 +0200

    Remove suspense

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react';
 import AppLayout from '@/components/layout/AppLayout';
 import { getIsAuthenticated } from '@/features/auth/auth-queries';
 import { AuthProvider } from '@/features/auth/components/AuthProvider';
-import { ProductListSkeleton } from '@/features/product/components/ProductList';
 import UserProfile, { UserProfileSkeleton } from '@/features/user/components/UserProfile';
 
 export default async function AuthLayout({ children, modal }: LayoutProps<'/'>) {
@@ -11,24 +10,11 @@ export default async function AuthLayout({ children, modal }: LayoutProps<'/'>) 
   return (
     <AuthProvider loggedIn={loggedIn}>
       <AppLayout headerContent={<Suspense fallback={<UserProfileSkeleton />}>{<UserProfile />}</Suspense>}>
-        <Suspense fallback={<AuthPageSkeleton />}>
+        <Suspense>
           {children}
           {modal}
         </Suspense>
       </AppLayout>
     </AuthProvider>
-  );
-}
-
-function AuthPageSkeleton() {
-  return (
-    <>
-      <div className="skeleton-animation mb-4 h-30 w-full rounded-lg" />
-      <div className="skeleton-animation mb-4 h-10 w-full rounded-lg" />
-      <div className="flex h-full grow flex-col gap-4">
-        <div className="skeleton-animation h-6 w-24 rounded" />
-        <ProductListSkeleton />
-      </div>
-    </>
   );
 }
