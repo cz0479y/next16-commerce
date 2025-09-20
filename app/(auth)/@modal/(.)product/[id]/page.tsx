ed8ad0ff7f4c19fb859c36b5d8999f0e837a27e1
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-20 23:23:15 +0200

    Dont cache modal?

--- app/(auth)/@modal/(.)product/[id]/page.tsx
+++ app/(auth)/@modal/(.)product/[id]/page.tsx
@@ -1,15 +1,8 @@
-'use cache: remote';
-
-import { cacheLife } from 'next/dist/server/use-cache/cache-life';
-import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
 import Product from '@/features/product/components/Product';
 
 export default async function ProductModal({ params }: PageProps<'/product/[id]'>) {
   const { id } = await params;
   const productId = Number(id);
-
-  cacheLife('max');
-  cacheTag('product-' + productId);
 
   return <Product imageClassName="h-60" productId={productId} />;
 }
