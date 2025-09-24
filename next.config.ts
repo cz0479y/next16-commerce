4a92201bfe3cbccb0b0aacd1408a64727c9b20a7
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-24 21:43:25 +0200

    Update client cache

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
     cacheComponents: true,
+    clientSegmentCache: true,
     inlineCss: true,
     reactCompiler: true,
     staleTimes: {
